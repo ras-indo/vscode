deeeb7ead58f38674150b56c0d8237dbb9895170
ras-indo <celengan1dana@gmail.com>
2026-01-20 19:30:12 +0700

    setup arm64 github actions build

--- src/vs/editor/browser/controller/editContext/native/nativeEditContext.ts
+++ src/vs/editor/browser/controller/editContext/native/nativeEditContext.ts
@@ -4,11 +4,14 @@
  *--------------------------------------------------------------------------------------------*/
 
 import './nativeEditContext.css';
-import { isFirefox } from '../../../../../base/browser/browser.js';
-import { addDisposableListener, getActiveElement, getWindow, getWindowId } from '../../../../../base/browser/dom.js';
+import { isFirefox, isIOS, isAndroid, isMobile } from '../../../../../base/browser/browser.js';
+import { addDisposableListener, getActiveElement, getWindow, getWindowId, scheduleAtNextAnimationFrame } from '../../../../../base/browser/dom.js';
 import { FastDomNode } from '../../../../../base/browser/fastDomNode.js';
 import { StandardKeyboardEvent } from '../../../../../base/browser/keyboardEvent.js';
+import { StandardMouseEvent } from '../../../../../base/browser/mouseEvent.js';
+import { Gesture } from '../../../../../base/browser/touch.js';
 import { KeyCode } from '../../../../../base/common/keyCodes.js';
+import { IDisposable } from '../../../../../base/common/lifecycle.js';
 import { IInstantiationService } from '../../../../../platform/instantiation/common/instantiation.js';
 import { EditorOption } from '../../../../common/config/editorOptions.js';
 import { EndOfLinePreference, IModelDeltaDecoration } from '../../../../common/model.js';
@@ -68,8 +71,19 @@ export class NativeEditContext extends AbstractEditContext {
 	private _targetWindowId: number = -1;
 	private _scrollTop: number = 0;
 	private _scrollLeft: number = 0;
+	private _selectionAndControlBoundsUpdateDisposable: IDisposable | undefined;
 
 	private readonly _focusTracker: FocusTracker;
+
+	// Untuk penanganan touch events
+	private _isTouchActive: boolean = false;
+	private _touchStartPosition: { x: number; y: number } | null = null;
+	private _touchStartTime: number = 0;
+	private _lastTapTime: number = 0;
+	private _lastTapPosition: { x: number; y: number } | null = null;
+	private _touchGesture: Gesture | undefined;
+	private _isScrolling: boolean = false;
+	private _touchMoveThreshold: number = 10;
 
 	constructor(
 		ownerID: string,
@@ -84,6 +98,12 @@ export class NativeEditContext extends AbstractEditContext {
 
 		this.domNode = new FastDomNode(document.createElement('div'));
 		this.domNode.setClassName(`native-edit-context`);
+		
+		this.domNode.setStyle('touch-action', 'manipulation');
+		this.domNode.setStyle('-webkit-tap-highlight-color', 'transparent');
+		this.domNode.setStyle('-webkit-user-select', 'none');
+		this.domNode.setStyle('user-select', 'none');
+		
 		this._imeTextArea = new FastDomNode(document.createElement('textarea'));
 		this._imeTextArea.setClassName(`ime-text-area`);
 		this._imeTextArea.setAttribute('readonly', 'true');
@@ -112,14 +132,13 @@ export class NativeEditContext extends AbstractEditContext {
 
 		this._screenReaderSupport = this._register(instantiationService.createInstance(ScreenReaderSupport, this.domNode, context, this._viewController));
 
+		this._touchGesture = new Gesture(this.domNode.domNode);
+
+		this._registerTouchEvents();
+
 		this._register(addDisposableListener(this.domNode.domNode, 'copy', (e) => {
 			this.logService.trace('NativeEditContext#copy');
 
-			// !!!!!
-			// This is a workaround for what we think is an Electron bug where
-			// execCommand('copy') does not always work (it does not fire a clipboard event)
-			// !!!!!
-			// We signal that we have executed a copy command
 			CopyOptions.electronBugWorkaroundCopyEventHasFired = true;
 
 			const copyEvent = createClipboardCopyEvent(e, /* isCut */ false, this._context, this.logService, isFirefox);
@@ -136,8 +155,6 @@ export class NativeEditContext extends AbstractEditContext {
 			if (cutEvent.isHandled) {
 				return;
 			}
-			// Pretend here we touched the text area, as the `cut` event will most likely
-			// result in a `selectionchange` event which we want to ignore
 			this._screenReaderSupport.onWillCut();
 			cutEvent.ensureClipboardGetsEditorData();
 			this.logService.trace('NativeEditContext#cut (before viewController.cut)');
@@ -217,18 +234,12 @@ export class NativeEditContext extends AbstractEditContext {
 		}));
 		this._register(editContextAddDisposableListener(this._editContext, 'compositionstart', (e) => {
 			this._updateEditContext();
-			// Utlimately fires onDidCompositionStart() on the editor to notify for example suggest model of composition state
-			// Updates the composition state of the cursor controller which determines behavior of typing with interceptors
 			this._viewController.compositionStart();
-			// Emits ViewCompositionStartEvent which can be depended on by ViewEventHandlers
 			this._context.viewModel.onCompositionStart();
 		}));
 		this._register(editContextAddDisposableListener(this._editContext, 'compositionend', (e) => {
 			this._updateEditContext();
-			// Utlimately fires compositionEnd() on the editor to notify for example suggest model of composition state
-			// Updates the composition state of the cursor controller which determines behavior of typing with interceptors
 			this._viewController.compositionEnd();
-			// Emits ViewCompositionEndEvent which can be depended on by ViewEventHandlers
 			this._context.viewModel.onCompositionEnd();
 		}));
 		let reenableTracking: boolean = false;
@@ -247,14 +258,273 @@ export class NativeEditContext extends AbstractEditContext {
 		this._register(NativeEditContextRegistry.register(ownerID, this));
 	}
 
-	// --- Public methods ---
+	private _registerTouchEvents(): void {
+		if (!isMobile) {
+			this._registerPointerEvents();
+			return;
+		}
+		
+		this._register(addDisposableListener(this.domNode.domNode, 'touchstart', (e) => {
+			this._handleTouchStart(e);
+		}, { passive: true }));
+
+		this._register(addDisposableListener(this.domNode.domNode, 'touchmove', (e) => {
+			this._handleTouchMove(e);
+		}, { passive: true }));
+
+		this._register(addDisposableListener(this.domNode.domNode, 'touchend', (e) => {
+			this._handleTouchEnd(e);
+		}, { passive: true }));
+
+		this._register(addDisposableListener(this.domNode.domNode, 'touchcancel', (e) => {
+			this._handleTouchCancel(e);
+		}, { passive: true }));
+
+		this._register(addDisposableListener(this.domNode.domNode, 'contextmenu', (e) => {
+			this._handleContextMenu(e);
+		}));
+
+		this._register(addDisposableListener(this.domNode.domNode, 'gesturestart', (e) => {
+			e.preventDefault();
+		}));
+
+		this._register(addDisposableListener(this.domNode.domNode, 'gesturechange', (e) => {
+			e.preventDefault();
+		}));
+
+		if (this._touchGesture) {
+			this._register(this._touchGesture.onTap((e) => this._handleTap(e)));
+			this._register(this._touchGesture.onDoubleTap((e) => this._handleDoubleTap(e)));
+			this._register(this._touchGesture.onLongPress((e) => this._handleLongPress(e)));
+		}
+	}
+
+	private _registerPointerEvents(): void {
+		this._register(addDisposableListener(this.domNode.domNode, 'pointerdown', (e) => {
+			this._handlePointerDown(e);
+		}));
+
+		this._register(addDisposableListener(this.domNode.domNode, 'pointermove', (e) => {
+			this._handlePointerMove(e);
+		}));
+
+		this._register(addDisposableListener(this.domNode.domNode, 'pointerup', (e) => {
+			this._handlePointerUp(e);
+		}));
+
+		this._register(addDisposableListener(this.domNode.domNode, 'pointercancel', (e) => {
+			this._handlePointerCancel(e);
+		}));
+
+		this._register(addDisposableListener(this.domNode.domNode, 'pointerleave', (e) => {
+			this._handlePointerLeave(e);
+		}));
+	}
+
+	private _handleTouchStart(e: TouchEvent): void {
+		if (e.touches.length > 1) {
+			return;
+		}
+
+		this._isTouchActive = true;
+		const touch = e.touches[0];
+		this._touchStartPosition = { x: touch.clientX, y: touch.clientY };
+		this._touchStartTime = Date.now();
+		this._isScrolling = false;
+
+		if (!this.isFocused()) {
+			this.focus();
+		}
+
+		const mouseEvent = this._createMouseEventFromTouch('mousedown', touch);
+		this._viewController.dispatchMouse(mouseEvent);
+	}
+
+	private _handleTouchMove(e: TouchEvent): void {
+		if (!this._isTouchActive || e.touches.length > 1) {
+			return;
+		}
+
+		const touch = e.touches[0];
+		
+		if (this._touchStartPosition) {
+			const deltaX = Math.abs(touch.clientX - this._touchStartPosition.x);
+			const deltaY = Math.abs(touch.clientY - this._touchStartPosition.y);
+			
+			if (deltaX > this._touchMoveThreshold || deltaY > this._touchMoveThreshold) {
+				this._isScrolling = true;
+				
+				const mouseEvent = this._createMouseEventFromTouch('mousemove', touch);
+				this._viewController.dispatchMouse(mouseEvent);
+				
+				this._touchStartPosition = { x: touch.clientX, y: touch.clientY };
+			}
+		}
+	}
+
+	private _handleTouchEnd(e: TouchEvent): void {
+		if (!this._isTouchActive) {
+			return;
+		}
+
+		this._isTouchActive = false;
+		
+		if (!this._isScrolling && this._touchStartPosition && e.changedTouches.length > 0) {
+			const touch = e.changedTouches[0];
+			const currentTime = Date.now();
+			const tapDuration = currentTime - this._touchStartTime;
+			
+			if (tapDuration < 300) {
+				this._handleTapAtPosition(touch.clientX, touch.clientY, currentTime);
+			}
+			
+			const mouseEvent = this._createMouseEventFromTouch('mouseup', touch);
+			this._viewController.dispatchMouse(mouseEvent);
+		}
+		
+		this._touchStartPosition = null;
+		this._isScrolling = false;
+	}
+
+	private _handleTouchCancel(e: TouchEvent): void {
+		this._isTouchActive = false;
+		this._touchStartPosition = null;
+		this._isScrolling = false;
+		
+		if (e.changedTouches.length > 0) {
+			const mouseEvent = this._createMouseEventFromTouch('mousecancel', e.changedTouches[0]);
+			this._viewController.dispatchMouse(mouseEvent);
+		}
+	}
+
+	private _handleTap(e: Event): void {
+		this.logService.trace('NativeEditContext#tap');
+		
+		if (!this.isFocused()) {
+			this.focus();
+		}
+	}
+
+	private _handleDoubleTap(e: Event): void {
+		this.logService.trace('NativeEditContext#doubleTap');
+		
+		if (!this.isFocused()) {
+			this.focus();
+		}
+		
+		this._viewController.wordSelect();
+	}
+
+	private _handleLongPress(e: Event): void {
+		this.logService.trace('NativeEditContext#longPress');
+		
+		if (!this.isFocused()) {
+			this.focus();
+		}
+		
+		this._viewController.startDragSelection();
+	}
+
+	private _handleTapAtPosition(x: number, y: number, currentTime: number): void {
+		if (this._lastTapTime && this._lastTapPosition) {
+			const timeDiff = currentTime - this._lastTapTime;
+			const distance = Math.sqrt(
+				Math.pow(x - this._lastTapPosition.x, 2) + 
+				Math.pow(y - this._lastTapPosition.y, 2)
+			);
+			
+			if (timeDiff < 500 && distance < 50) {
+				this._handleDoubleTap(new Event('dbltap'));
+				this._lastTapTime = 0;
+				this._lastTapPosition = null;
+				return;
+			}
+		}
+		
+		this._lastTapTime = currentTime;
+		this._lastTapPosition = { x, y };
+	}
+
+	private _handleContextMenu(e: Event): void {
+		if (isMobile) {
+			e.preventDefault();
+			this._viewController.showContextMenu();
+		}
+	}
+
+	private _createMouseEventFromTouch(type: string, touch: Touch): StandardMouseEvent {
+		const mouseEventInit: MouseEventInit = {
+			clientX: touch.clientX,
+			clientY: touch.clientY,
+			screenX: touch.screenX,
+			screenY: touch.screenY,
+			button: 0,
+			buttons: 1,
+			ctrlKey: false,
+			shiftKey: false,
+			altKey: false,
+			metaKey: false
+		};
+		
+		const mouseEvent = new MouseEvent(type, mouseEventInit);
+		return new StandardMouseEvent(mouseEvent);
+	}
+
+	private _handlePointerDown(e: PointerEvent): void {
+		if (isMobile) return;
+        
+		const mouseEvent = new StandardMouseEvent(e);
+		this._viewController.dispatchMouse(mouseEvent);
+		
+		if (e.button === 0) {
+			e.preventDefault();
+			e.stopPropagation();
+		}
+	}
+
+	private _handlePointerMove(e: PointerEvent): void {
+		if (isMobile) return;
+		
+		const mouseEvent = new StandardMouseEvent(e);
+		this._viewController.dispatchMouse(mouseEvent);
+		
+		if (e.buttons & 1) {
+			e.preventDefault();
+			e.stopPropagation();
+		}
+	}
+
+	private _handlePointerUp(e: PointerEvent): void {
+		if (isMobile) return;
+		
+		const mouseEvent = new StandardMouseEvent(e);
+		this._viewController.dispatchMouse(mouseEvent);
+		
+		if (e.button === 0) {
+			e.preventDefault();
+			e.stopPropagation();
+		}
+	}
+
+	private _handlePointerCancel(e: PointerEvent): void {
+		if (isMobile) return;
+		
+		this._viewController.dispatchMouse(new StandardMouseEvent(e));
+	}
+
+	private _handlePointerLeave(e: PointerEvent): void {
+		if (isMobile) return;
+	}
 
 	public override dispose(): void {
-		// Force blue the dom node so can write in pane with no native edit context after disposal
+		this._touchGesture?.dispose();
+		
 		this.domNode.domNode.editContext = undefined;
 		this.domNode.domNode.blur();
 		this.domNode.domNode.remove();
 		this._imeTextArea.domNode.remove();
+		this._selectionAndControlBoundsUpdateDisposable?.dispose();
+		this._selectionAndControlBoundsUpdateDisposable = undefined;
 		super.dispose();
 	}
 
@@ -262,8 +532,6 @@ export class NativeEditContext extends AbstractEditContext {
 		this._screenReaderSupport.setAriaOptions(options);
 	}
 
-	/* Last rendered data needed for correct hit-testing and determining the mouse position.
-	 * Without this, the selection will blink as incorrect mouse position is calculated */
 	public getLastRenderData(): Position | null {
 		return this._primarySelection.getPosition();
 	}
@@ -295,7 +563,6 @@ export class NativeEditContext extends AbstractEditContext {
 	}
 
 	public override onDecorationsChanged(e: ViewDecorationsChangedEvent): boolean {
-		// true for inline decorations that can end up relayouting text
 		return true;
 	}
 
@@ -359,8 +626,6 @@ export class NativeEditContext extends AbstractEditContext {
 
 	public focus(): void {
 		this._focusTracker.focus();
-
-		// If the editor is off DOM, focus cannot be really set, so let's double check that we have managed to set the focus
 		this.refreshFocusState();
 	}
 
@@ -368,8 +633,6 @@ export class NativeEditContext extends AbstractEditContext {
 		this._focusTracker.refreshFocusState();
 	}
 
-	// TODO: added as a workaround fix for https://github.com/microsoft/vscode/issues/229825
-	// When this issue will be fixed the following should be removed.
 	public setEditContextOnDomNode(): void {
 		const targetWindow = getWindow(this.domNode.domNode);
 		const targetWindowId = getWindowId(targetWindow);
@@ -379,8 +642,6 @@ export class NativeEditContext extends AbstractEditContext {
 		}
 	}
 
-	// --- Private methods ---
-
 	private _onKeyUp(e: KeyboardEvent) {
 		inputLatency.onKeyUp();
 		this._viewController.emitKeyUp(new StandardKeyboardEvent(e));
@@ -389,7 +650,6 @@ export class NativeEditContext extends AbstractEditContext {
 	private _onKeyDown(e: KeyboardEvent) {
 		inputLatency.onKeyDown();
 		const standardKeyboardEvent = new StandardKeyboardEvent(e);
-		// When the IME is visible, the keys, like arrow-left and arrow-right, should be used to navigate in the IME, and should not be propagated further
 		if (standardKeyboardEvent.keyCode === KeyCode.KEY_IN_COMPOSITION) {
 			standardKeyboardEvent.stopPropagation();
 		}
@@ -399,6 +659,18 @@ export class NativeEditContext extends AbstractEditContext {
 	private _updateDomAttributes(): void {
 		const options = this._context.configuration.options;
 		this.domNode.domNode.setAttribute('tabindex', String(options.get(EditorOption.tabIndex)));
+		
+		if (isMobile) {
+			this.domNode.setAttribute('inputmode', 'text');
+			
+			if (isIOS) {
+				const autoClosingBrackets = options.get(EditorOption.autoClosingBrackets);
+				const autoClosingQuotes = options.get(EditorOption.autoClosingQuotes);
+				if (!autoClosingBrackets && !autoClosingQuotes) {
+					this.domNode.setAttribute('autocorrect', 'off');
+				}
+			}
+		}
 	}
 
 	private _updateEditContext(): void {
@@ -527,7 +799,18 @@ export class NativeEditContext extends AbstractEditContext {
 		}
 	}
 
-	private _updateSelectionAndControlBoundsAfterRender() {
+	private _updateSelectionAndControlBoundsAfterRender(): void {
+		if (this._selectionAndControlBoundsUpdateDisposable) {
+			return;
+		}
+		const targetWindow = getWindow(this.domNode.domNode);
+		this._selectionAndControlBoundsUpdateDisposable = scheduleAtNextAnimationFrame(targetWindow, () => {
+			this._selectionAndControlBoundsUpdateDisposable = undefined;
+			this._applySelectionAndControlBounds();
+		});
+	}
+
+	private _applySelectionAndControlBounds(): void {
 		const options = this._context.configuration.options;
 		const contentLeft = options.get(EditorOption.layoutInfo).contentLeft;
 
@@ -535,7 +818,6 @@ export class NativeEditContext extends AbstractEditContext {
 		const verticalOffsetStart = this._context.viewLayout.getVerticalOffsetForLineNumber(viewSelection.startLineNumber);
 		const verticalOffsetEnd = this._context.viewLayout.getVerticalOffsetAfterLineNumber(viewSelection.endLineNumber);
 
-		// Make sure this doesn't force an extra layout (i.e. don't call it before rendering finished)
 		const parentBounds = this._parent.getBoundingClientRect();
 		const top = parentBounds.top + verticalOffsetStart - this._scrollTop;
 		const height = verticalOffsetEnd - verticalOffsetStart;
